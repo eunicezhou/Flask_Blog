835860e2e6eebd3574e45d630eb2b22519670d23
eunicezhou <zizhen0826@gmail.com>
2024-04-30 14:19:22 +0800

    :writing_hand: feat: 加入新增留言的功能

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,9 +1,28 @@
 from flask import Blueprint,render_template
-from flask import request
+from flask import request, redirect, url_for
 from models import Post, session, Tag
+from .forms import PostForm
+from app import db
 
 posts = Blueprint('posts', __name__, 
                   template_folder= 'templates')
+
+@posts.route('/create', methods = ['POST', 'GET'])
+def post_create():
+    form = PostForm()
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        body = request.form.get('body')
+        try:
+            post = Post(title = title, body = body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('traceback')
+        return redirect(url_for('posts.post_detail', slug = post.slug))
+    
+    return render_template('posts/post_create.html', form = form)
 
 @posts.route('/')
 def posts_list():
@@ -12,7 +31,7 @@ def posts_list():
     if q:
         posts = session.query(Post).filter(Post.title.contains(q) | Post.body.contains(q)).all()
     else:
-        posts = session.query(Post).all()
+        posts = session.query(Post).order_by(Post.created.desc())
     return render_template('posts/posts.html', posts = posts)
 
 @posts.route('/<slug>')
